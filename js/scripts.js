a5982ee97f5eefd8578da0ff3d403f59ce7e3de7
Benjamin Herson <benjaminherson@gmail.com>
2015-02-20 10:53:54 -0800

    add Pizza prototype with slices method

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,16 @@
-var BankAccount = {
-  balance: 0,
-  deposit: function(amount) {
-    this.balance = this.balance + amount;
-  },
-  withdraw: function(amount) {
-    if (this.balance - amount < 0) {
-      alert("Cannot squeeze blood out of a turnip!")
-    } else if (amount > 500) {
-      alert("Money does not grow on trees! Goddamit!")
+var Pizza = {
+  diameter: 0,
+  topping: "cheese",
+  slices: function() {
+    var factor;
+    if (this.diameter > 48) {
+      alert("Sorry, how about ordering multiple pies instead.");
+    } else if (this.topping === "pepperoni") {
+      factor = .7
     } else {
-      this.balance = this.balance - amount;
+      factor = .625
     }
+    var howMany = Math.round(this.diameter * factor)
+    return howMany;
   }
 };
-
-
-$(document).ready(function() {
-  $("form#signUpForm").submit(function(event) {
-    event.preventDefault();
-    var name = $("input#name").val();
-    var initialDeposit = parseFloat($("input#initialDeposit").val());
-
-    var newBankAccount = Object.create(BankAccount);
-    newBankAccount.name = name;
-    newBankAccount.deposit(initialDeposit);
-
-    $(".accountHolder").text(newBankAccount.name);
-    $(".currentBalance").text(newBankAccount.balance);
-    $("#balance").show();
-
-    name = $("input#name").val("");
-    initialDeposit = $("input#initialDeposit").val("");
-
-    $("form#transferForm").submit(function(event) {
-      event.preventDefault();
-      var inputtedDeposit = parseFloat($("input#deposit").val());
-      var inputtedWithdraw = parseFloat($("input#withdraw").val());
-
-      if (isNaN(inputtedWithdraw)) {
-        newBankAccount.deposit(inputtedDeposit);
-      } else if (isNaN(inputtedDeposit)) {
-        newBankAccount.withdraw(inputtedWithdraw);
-      } else {
-        newBankAccount.deposit(inputtedDeposit);
-        newBankAccount.withdraw(inputtedWithdraw);
-      }
-
-      $(".currentBalance").text(newBankAccount.balance);
-
-      inputtedDeposit = $("input#deposit").val("");
-      inputtedWithdraw = $("input#withdraw").val("");
-
-    });
-  });
-});
--- specs/specs.js
+++ specs/specs.js
@@ -1,1 +1,34 @@
-describe()
+describe("Pizza", function() {
+  describe("slices", function() {
+    it("will recognize that there can be no slices for a 0-diameter pizza", function() {
+      var newPizza = Object.create(Pizza);
+      newPizza.diameter = 0;
+      expect(newPizza.slices()).to.equal(0);
+    });
+
+    it("will return the number of (perfect) slices with 5-inch long crusts", function() {
+      var newPizza = Object.create(Pizza);
+      newPizza.diameter = 8;
+      expect(newPizza.slices()).to.equal(5);
+    });
+
+    it("will return the number of slices when the pizza won't divide perfectly into slices with 5-inch crusts", function() {
+      var newPizza = Object.create(Pizza);
+      newPizza.diameter = 12;
+      expect(newPizza.slices()).to.equal(8);
+    });
+
+    it("will reject a pizza order of a pizza larger than our 48-inch oven", function() {
+      var newPizza = Object.create(Pizza);
+      newPizza.diameter = 50;
+      expect(newPizza.slices()).to.eql(NaN);
+    });
+
+    it("will return the number of smaller slices for a denser pepperoni pizza", function() {
+      var newPizza = Object.create(Pizza);
+      newPizza.diameter = 20;
+      newPizza.topping = "pepperoni";
+      expect(newPizza.slices()).to.equal(14);
+    });
+  });
+});
