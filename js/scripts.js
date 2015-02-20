e4bfed82afcc23b9587a65c914ebdd9de5dca985
Benjamin Herson <benjaminherson@gmail.com>
2015-02-20 12:37:57 -0800

    add most of cost method

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,36 @@ var Pizza = {
   topping: "Cheese",
   slices: function() {
     var factor;
-    if (this.diameter > 48) {
-      alert("Sorry, how about ordering multiple pies instead.");
-    } else if (this.topping === "Pepperoni") {
+    if (this.topping === "Pepperoni") {
       factor = .7
     } else {
       factor = .625
     }
     var howMany = Math.round(this.diameter * factor)
     return howMany;
+  },
+  cost: function() {
+    var total;
+    var area = .8 * this.diameter * this.diameter;
+    var extra = 0;
+    if (this.topping === "Pepperoni") {
+      extra = 1
+    }
+
+    if (this.diameter <= 0) {
+      total = 0
+    } else if (this.diameter <= 8) {
+      total = 5.99;
+    } else if (this.diameter <= 16) {
+      total = 5 + (area * .05);
+    } else if (this.diameter <= 24) {
+      total = 7.5 + (area * .05);
+    } else {
+      total = 3 + (area * .045);
+    }
+    total = total + extra;
+    var newTotal = (total).toFixed(2)
+    return parseFloat(newTotal);
   }
 };
 
@@ -27,7 +48,7 @@ $(document).ready(function() {
     nextPizza.topping = inputtedTopping;
 
     $(".response").hide();
-    if (isNaN(nextPizza.slices())) {
+    if (nextPizza.diameter > 48) {
       $("#ovenDisclaimer1").show();
     } else if (nextPizza.diameter < 0) {
       $("#ovenDisclaimer2").show();
--- specs/specs.js
+++ specs/specs.js
@@ -18,17 +18,51 @@ describe("Pizza", function() {
       expect(newPizza.slices()).to.equal(8);
     });
 
-    it("will reject a pizza order of a pizza larger than our 48-inch oven", function() {
-      var newPizza = Object.create(Pizza);
-      newPizza.diameter = 50;
-      expect(newPizza.slices()).to.eql(NaN);
-    });
-
     it("will return the number of smaller slices for a denser pepperoni pizza", function() {
       var newPizza = Object.create(Pizza);
       newPizza.diameter = 20;
       newPizza.topping = "Pepperoni";
       expect(newPizza.slices()).to.equal(14);
+    });
+  });
+
+  describe("size", function() {
+    
+  });
+
+  describe("cost", function() {
+    it("will not charge you if your pizza does not exist", function() {
+      var newPizza = Object.create(Pizza);
+      newPizza.diameter = 0;
+      expect(newPizza.cost()).to.equal(0.00);
+    });
+    it("will return the price of our personal pan special for any pizza 8 inches or less", function() {
+      var newPizza = Object.create(Pizza);
+      newPizza.diameter = 6;
+      expect(newPizza.cost()).to.equal(5.99);
+    });
+    it("will return the price of a medium pizza (up to 16 inches)", function() {
+      var newPizza = Object.create(Pizza);
+      newPizza.diameter = 12;
+      expect(newPizza.cost()).to.equal(10.76);
+    })
+    it("will return the price of a large pizza (up to 24 inches)", function() {
+      var newPizza = Object.create(Pizza);
+      newPizza.diameter = 18;
+      expect(newPizza.cost()).to.equal(20.46);
+    });
+
+    it("will return the price of our speciality, the jumbo pizza (up to 48 inches)", function() {
+      var newPizza = Object.create(Pizza);
+      newPizza.diameter = 30;
+      expect(newPizza.cost()).to.equal(35.40);
+    });
+
+    it("will return the price for a medium pepperoni pie", function() {
+      var newPizza = Object.create(Pizza);
+      newPizza.diameter = 10.5;
+      newPizza.topping = "Pepperoni";
+      expect(newPizza.cost()).to.equal(10.41);
     });
   });
 });
